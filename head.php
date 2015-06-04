81eb01b7a0cb3c9221ec78f81db56afb861efef2
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-06-05 03:08:22 +0600

    uedit

--- head.php
+++ head.php
@@ -14,8 +14,8 @@
 <meta name="author" content="Jahidul Hamid" >
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="style/mycss.css" />
-<!--<link rel="stylesheet" href="style/mycss.min.css" />-->
+<!--<link rel="stylesheet" href="style/mycss.css" />-->
+<link rel="stylesheet" href="style/mycss.min.css" />
 <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
