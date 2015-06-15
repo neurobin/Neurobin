436728983010621ae0e5aff4c56604ab38b1f05d
mdjahidulhamid <jahidulhamid@yahoo.com>
2015-06-15 07:50:30 +0600

    uedit

--- uedit/showinfo.php
+++ uedit/showinfo.php
@@ -5,36 +5,7 @@
 		<?php chdir("../"); ?>
 
 
-<script>
-	if (window.location.protocol != "http:")
-		window.location.href = "http:" + window.location.href.substring(window.location.protocol.length);
-</script>
-
-<link href="http://gmpg.org/xfn/11" rel="profile">
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
-
-<!-- CSS -->
-
-<meta name="author" content="Jahidul Hamid" >
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="style/mycss.css" />
-<!--<link rel="stylesheet" href="style/mycss.min.css" />-->
-<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-<script src="script/jquery.min.1.7.1.custom.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!--<script src="script/myjs.js"></script>-->
-<script src="script/myjs.min.js"></script>
-
-
-<link rel="stylesheet" href="Highlightjs/styles/github.css">
-<script src="Highlightjs/highlight.pack.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-
-<link rel="shortcut icon" href="img/logo48.png" type="image/x-icon" />
+<?php require_once('statichead.php'); ?>
 
 
 		<meta name="description" content="Universal Text Editor User Manual">
